9882d0e793febf879488a360e56df2b3549c61e2
Sadık Orak <muhammedsadikorak@gmail.com>
2024-08-10 15:08:44 +0300

    Tag ve listablecontent sınıfı eklendi

---
+++ src/NewsManagement.Domain/Entities/ListableContents/ListableContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace NewsManagement.Entities.ListableContents
+{
+  public class ListableContent : FullAuditedAggregateRoot<int>
+  {
+    public string Title { get; set; }
+    public string Spot { get; set; }
+    public bool Status { get; set; }
+    public DateTime PublishTime { get; set; }
+  }
+}
---
+++ src/NewsManagement.Domain/Entities/Tag/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsManagement.Entities.ListableContents;
+
+namespace NewsManagement.Entities.Tag
+{
+  public class Tag : ListableContent
+  {
+    public string Name { get; set; }
+  }
+}
